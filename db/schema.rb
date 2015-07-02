102ffda0e87af5a0c03301cc89b1c3dd8bd4725b
“wwwwin” <newlife948@gmail.com>
2015-07-02 21:05:45 +0800

    增加使用者姓名欄位

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702121322) do
+ActiveRecord::Schema.define(version: 20150702124557) do
 
   create_table "topics", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20150702121322) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
