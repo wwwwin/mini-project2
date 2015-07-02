a98d0b45587b28d32d7cd446e454cf5cb005ee3a
“wwwwin” <newlife948@gmail.com>
2015-07-02 11:40:40 +0800

    rake topic db

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,43 @@
 class TopicsController < ApplicationController
+
+before_action :
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+
+  def destroy
+
+  end
+
+  private
+
+  def topic_params
+
+    params.require(:topics)
+
+  end
+
+
 end
