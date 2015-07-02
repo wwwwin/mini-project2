8788ac633e133898043cbeab14c1ba993f5e58a9
“wwwwin” <newlife948@gmail.com>
2015-07-02 12:15:36 +0800

    增加schema的欄位

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,10 @@
 ActiveRecord::Schema.define(version: 20150701105621) do
 
   create_table "topics", force: :cascade do |t|
+
+    t.string :topic
+    t.text :article
+
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
